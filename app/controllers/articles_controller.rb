5763e4b89adc90ccbc227739828ccb985dbf492e
TusharVaniya <vaniyatushar@gmail.com>
2019-04-12 10:59:35 +0530

    Adding all other functionality with Admin also

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,19 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:edit,:update,:destroy,:show]
+  before_action :require_user,except: [:index , :show]
+  before_action :require_same_user, only: [:edit, :update]
+
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page],per_page: 5)
   end
 
   def new
     @article = Article.new()
   end
   def create
-    #render plain: params[:article].inspect #simply display karva mate
-
-=begin
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
-=end
-
-    @article = Article.new(article_params)
+    @article.user = current_user
     if @article.save
       flash[:success] = 'Article was successfully created'
       redirect_to article_path(@article)
@@ -56,5 +52,11 @@ class ArticlesController < ApplicationController
   end
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+  def require_same_user
+    if current_user!= @article.user and !current_user.admin?
+      flash[:danger] = "You can edit or delete your own articles"
+      redirect_to root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/new'
+  get 'users_controller/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 =begin
   get 'page/home', to: 'page#home'
@@ -8,5 +13,17 @@ Rails.application.routes.draw do
   root 'page#home'
   get 'about', to: 'page#about'
 
+
   resources :articles
+  get 'signup',to: 'users#new'
+  post 'users',to: 'users#create'
+
+  resources :users
+  post 'edit',to: 'users#edit'
+  get 'update',to: 'users#update'
+
+  get 'login',to: 'sessions#new'
+  post 'login',to: 'sessions#create'
+  delete 'logout',to: 'sessions#destroy'
+
 end
